6e4bbc97a99cee2d5d6ac7c05c7a876323b2d462
Phan Van khoi <vothien10101996@gmail.com>
2018-01-05 15:28:08 +0700

    relationships model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,7 @@
 class Food < ApplicationRecord
+  has_many :food_orders
+  has_many :orders, through: :food_orders
+  has_many :reviews, dependent: :destroy
+  belongs_to :discount
+  belongs_to :category
 end
