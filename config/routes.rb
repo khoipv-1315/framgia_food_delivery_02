213821addc3a7125a93bd768f9ac1e32e0a973f2
Phan Van khoi <vothien10101996@gmail.com>
2018-01-15 11:47:31 +0700

    static pages admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "contact" => "customers/static_pages#contact"
-  get "error" => "customers/static_pages#error" 
+  get "error" => "customers/static_pages#error"
+  namespace :admin do
+    root "dashboard#index"
+  end
 end
